4d819b2c65ca9e4a56a4bd7bedab07f008717013
Ayush <ayushpanchal005@gmail.com>
2023-06-24 21:27:05 +0530

    Separated the components in different files

---
+++ src/books.js
@@ -0,0 +1,23 @@
+import img2 from './images/book-2.jpg'; // importing image from the src folder
+import img3 from './images/book-3.jpg';
+
+export const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: './images/book-1.jpg', // importing image from public folder
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'David Goggins',
+    title: "Can't Hurt Me: Master Your Mind and Defy the Odds",
+    img: img3,
+    id: 3,
+  },
+];
